4a9008285dda435d4c58c7a175047edd95601436
jh0222 <gihyuny0222@gmail.com>
2024-05-30 22:43:37 +0900

    light&dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import Layout from "./components/layout/Layout";
+import Home from "./pages/Home";
+import { GlobalStyle } from "./style/global";
+import { ThemeName, dark, getTheme, light } from "./style/theme";
+import ThemeSwitcher from "./components/header/ThemeSwitcher";
+import { useContext, useState } from "react";
+import { BookStoreThemeProvider, ThemeContext } from "./context/themeContext";
 
 function App() {
+  // const [themeName, setThemeName] = useState<ThemeName>("light");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BookStoreThemeProvider>
+      <ThemeSwitcher/>
+      <Layout>
+        <Home />
+      </Layout>
+    </BookStoreThemeProvider>
+    </>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <>
+      <div>book store</div>
+    </>
+  )
+  
+}
+
+export default Home;
